a97b45720b2d322433bc875ea27bc7d08868fedb
Yacine <yacine.ayachi@outlook.fr>
2024-10-27 13:26:58 +0100

    added data to same folder

--- code.py
+++ code.py
@@ -1,3 +1,5 @@
+
+# DATA_URL = ('https://univlyon2fr-my.sharepoint.com/:f:/g/personal/yayachi_univ-lyon2_fr/EmWXiTs2d_xGt573tGCT52YBPJ5GhnbINEbjv9gONCx_3g?e=mU2xYP')
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -5,31 +7,9 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://univlyon2fr-my.sharepoint.com/:f:/g/personal/yayachi_univ-lyon2_fr/EmWXiTs2d_xGt573tGCT52YBPJ5GhnbINEbjv9gONCx_3g?e=mU2xYP')
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+data = pd.read_csv('neufs.csv')
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
